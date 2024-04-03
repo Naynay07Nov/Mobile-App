6b59cad0f92082ae9b56f1632270a274e3a9dfc3
Nnayjya <pichamol07@gmail.com>
2024-04-03 12:38:30 +0700

    Change

--- components/week3/MyIcon.js
+++ components/week3/MyIcon.js
@@ -5,11 +5,11 @@ import { FontAwesome } from '@expo/vector-icons';
 
 export default function MyIcon(props) {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems : 'center'}}>
-             <TouchableOpacity style={{ alignItems : "center" }} onPress={ props.onPress }>     
-            <FontAwesome name={ props.name }  size={ props.size } color={ props.color='orange' } />
-          <Text>{ props.title }</Text>
-          </TouchableOpacity>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <TouchableOpacity style={{ alignItems: "center" }} onPress={props.onPress}>
+                <FontAwesome name={props.name} size={props.size} color={props.color = 'orange'} />
+                <Text>{props.title}</Text>
+            </TouchableOpacity>
         </View>
     );
 }
--- navigations/RootStack.js
+++ navigations/RootStack.js
@@ -2,6 +2,11 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import BottomTab from "./BottomTab";
 import PokemonTab from "./PokemonTab";
+import Travel from "../screens/week3/Travel";
+import YourHealth from "../screens/week9/YourHealth";
+import TodoList from "../screens/week9/TodoList";
+import Location from "../screens/week10/Location";
+import LocationQuiz from "../screens/week10/LocationQuiz";
 
 const Stack = createStackNavigator();
 
@@ -10,6 +15,10 @@ export default function RootStack() {
     <Stack.Navigator initialRouteName="BottomTab">
       <Stack.Screen name="BottomTab" component={BottomTab} options={{ headerShown: false }} />
       <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title : "Pokemon World" }}  />
+      <Stack.Screen name="YourHealth" component={YourHealth} />
+      <Stack.Screen name="TodoList" component={TodoList} />
+      <Stack.Screen name="Location" component={Location} />
+      <Stack.Screen name="LocationQuiz" component={LocationQuiz} />
     </Stack.Navigator>
   );
 }
